d9c0743d2b5f630df7019ba7ef119592ba720058
Shubham Kumar Sinha <106798097+Shubh27129@users.noreply.github.com>
2023-05-19 01:09:40 +0530

    Main Code

---
+++ Registrn-page-bootstrap/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    console.log(document.getElementById("fname").value);
+    console.log(document.getElementById("mail").value);
+    console.log(document.getElementById("num").value);
+    console.log(document.getElementById("date").value);
+    console.log(document.getElementById("time").value);
+}
